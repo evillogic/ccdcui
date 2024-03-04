9fbfb41e2cbc0862875baa4ec302d33ae104f4c0
Daylam Tayari <git@tayari.gg>
2024-03-03 21:27:17 -0600

    Added report creation API route

--- src/app/api/reports/route.ts
+++ src/app/api/reports/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { fetchReports } from '@/utils/mongo';
+import { fetchReports, insertReport } from '@/utils/mongo';
 
 export async function GET() {
     const reports = await fetchReports();
@@ -9,4 +9,19 @@ export async function GET() {
             'Content-Type': 'application/json',
         }
     })
+}
+
+export async function POST(req: Request) {
+    try {
+        const report = await req.json();
+        await insertReport(report);
+        return new NextResponse(JSON.stringify({ message: 'Successfully created report' }), {
+            status: 200,
+        })
+    } catch (error) {
+        console.error('Report API creation failed:', error);
+        return new NextResponse(JSON.stringify({ message: 'Failed to create report' }), {
+            status: 500,
+        })
+    }
 }
