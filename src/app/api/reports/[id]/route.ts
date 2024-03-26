fa0697d97f73ba51231ec31febe83ed19759c3c7
Daylam Tayari <git@tayari.gg>
2024-03-25 22:38:02 -0500

    Implemented report deletion endpoint

--- src/app/api/reports/[id]/route.ts
+++ src/app/api/reports/[id]/route.ts
@@ -7,7 +7,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const query = { _id: id}
         const reports = await fetchReports(query);
         res.status(200).json({ reports })
-    } else {
+    } else if (req.method == 'DELETE') {
+        const id = req.query.id
+        const query = { _id: id}
+        const reports = await fetchReports(query);
+        if reports === [] {
+          res.status(404).json({ reports })
+        } else {
+          const deletion = await deleteReports(reports);
+          res.status(200).json({ deletion })
+        }
+    } {
         res.status(405).json({ message: 'Method Not Allowed'})
     }
 }
