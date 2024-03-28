9b1ba01b88e4744b34f8f108477c2747774965f8
Daylam Tayari <git@tayari.gg>
2024-03-27 23:43:54 -0500

    Implemented report submission into submit api endpoint

--- src/app/api/reports/[id]/submit/route.ts
+++ src/app/api/reports/[id]/submit/route.ts
@@ -5,7 +5,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method == 'GET') {
         const id = req.query.id
         const query = { _id: id}
-        // TODO Implement report submission function
+        const report = await fetchReports(query);
+        if report === [] {
+            res.status(404).json({ reports })
+        } else {
+            var submit = await submitReport(report);
+            res.status(200).json({ submit })
+        }
         res.status(200).json({ ok })
     } else {
         res.status(405).json({ message: 'Method Not Allowed'})
